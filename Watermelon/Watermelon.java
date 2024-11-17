3f127dbe8de11e7235cd76230e2a286abd0c67f5
Ameed Darawsheh <s12240153@stu.najah.edu>
2024-11-17 10:26:29 +0200

    Create Watermelon.java

---
+++ Watermelon/Watermelon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Watermelon{
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int test = scan.nextInt();
+        if(test%2==0%%test>2){
+            System.out.print("YES");
+            return;
+        }
+        System.out.print("NO");
+    }
+}
