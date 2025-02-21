edfcbe6209916a14510a8ea762ebfa75ce2fc88c
Ameed Darawsheh <s12240153@stu.najah.edu>
2025-02-21 10:48:45 +0200

    Update Watermelon.java

--- Watermelon/Watermelon.java
+++ Watermelon/Watermelon.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class Watermelon{
-    public static void main (String[] args){
-        Scanner scan = new Scanner(System.in);
-        int test = scan.nextInt();
-        if(test%2==0%%test>2){
-            System.out.print("YES");
-            return;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int inputuser = input.nextInt();
+        input.close();
+        if (inputuser % 2 == 0 && inputuser > 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-        System.out.print("NO");
     }
 }
